df7444202876aae04ba4c7dbc62495c878c5abbb
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2020-12-16 15:59:00 +0000

    Added rspec files and updated fizzbuzz program logic to pass all test cases

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,12 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end  it 'returns 19 for the number 19' do
+      expect(19.fizzbuzz).to eq 19
+  end
 end
